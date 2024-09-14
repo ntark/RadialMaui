c588f07c23615ed2306a06f288c054b855044b86
ntark <ntark20@freeuni.edu.ge>
2024-09-14 20:58:31 +0400

    fix android permissions

--- Platforms/Android/Services/FileSaveService.cs
+++ Platforms/Android/Services/FileSaveService.cs
@@ -9,8 +9,27 @@ namespace RadialMaui.Platforms
     {
         public string GetDownloadPath()
         {
-            var folder = Environment.GetExternalStoragePublicDirectory(Environment.DirectoryDownloads);
-            
+            var writeStatus = Permissions.CheckStatusAsync<Permissions.StorageWrite>().GetAwaiter().GetResult();
+
+            if (writeStatus != PermissionStatus.Granted)
+            {
+                writeStatus = Permissions.RequestAsync<Permissions.StorageWrite>().GetAwaiter().GetResult();
+            }
+
+            var readStatus = Permissions.CheckStatusAsync<Permissions.StorageRead>().GetAwaiter().GetResult();
+
+            if (readStatus != PermissionStatus.Granted)
+            {
+                readStatus = Permissions.RequestAsync<Permissions.StorageRead>().GetAwaiter().GetResult();
+            }
+
+            if (readStatus != PermissionStatus.Granted || writeStatus != PermissionStatus.Granted)
+            {
+                throw new Exception("No rights to save to file storage");
+            }
+
+            var folder = Android.App.Application.Context.GetExternalFilesDir(Android.OS.Environment.DirectoryDocuments);
+
             if (folder == null)
             {
                 throw new Exception("download folder not found");
